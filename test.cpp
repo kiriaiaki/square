addfe9f06f412db7ec350afe8b56a5ab2c42f981
Кирилл <kiriaiaki@MacBook-Air-Kirill.local>
2025-08-26 20:12:15 +0700

    before makefile

--- test.cpp
+++ test.cpp
@@ -2,13 +2,14 @@
 
 void Testing ()
 {
-    Test tests [5] =
+    Test tests [6] =
     {
-        {.A = 345, .B = 2, .C = 12, .NSolve = ZERO_ROOTS, .X1 = 0, .X2 = 0},
-        {.A = 3, .B = 6, .C = 3, .NSolve = ONE_ROOT, .X1 = -1, .X2 = -1},
-        {.A = 1, .B = -5, .C = 6, .NSolve = TWO_ROOTS, .X1 = 3, .X2 = 2},
-        {.A = 0, .B = 0, .C = 0, .NSolve = INFINITE_ROOTS, .X1 = 0, .X2 = 0},
-        {.A = 0, .B = 0, .C = 4, .NSolve = IMPOSSIBLE_ROOTS, .X1 = 0, .X2 = 0},
+        {.A = 345,  .B = 2,          .C = 12,     .NSolve = ZERO_ROOTS,       .X1 = 0,          .X2 = 0         },
+        {.A = 12.5, .B = 2345.87654, .C = 76.693, .NSolve = TWO_ROOTS,        .X1 = -0.0326984, .X2 = -187.637  },
+        {.A = 3,    .B = 6,          .C = 3,      .NSolve = ONE_ROOT,         .X1 = -1,         .X2 = -1        },
+        {.A = 1,    .B = -5,         .C = 6,      .NSolve = TWO_ROOTS,        .X1 = 3,          .X2 = 2         },
+        {.A = 0,    .B = 0,          .C = 0,      .NSolve = INFINITE_ROOTS,   .X1 = 0,          .X2 = 0         },
+        {.A = 0,    .B = 0,          .C = 4,      .NSolve = IMPOSSIBLE_ROOTS, .X1 = 0,          .X2 = 0         },
     };
 
     const int len = sizeof(tests) / sizeof(tests[0]);
